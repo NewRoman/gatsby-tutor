e6f667a5ac07c3be5ab7fd04deb6b1c08c02a873
roman.horbunov <roman.horbunov@rolique.io>
2023-01-05 14:11:46 +0200

    Add blogs list page

---
+++ src/pages/blog.js
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+      </ul>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
+
+export const Head = (props) => {
+  console.log('props', props)
+  return <Seo title="My Blog Posts" />
+}
+
+
+export default BlogPage
